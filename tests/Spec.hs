554f38e5abf2694827f644cc9fe5d54d162e429f
Mark Wotton <mwotton@gmail.com>
2026-07-01 12:54:02 +0700

    Clarify explicit test spec list

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -4,4 +4,5 @@ import qualified Data.JudySpec
 import           Test.Hspec   (hspec)
 
 main :: IO ()
+-- Keep this explicit list in sync with any future *Spec modules.
 main = hspec Data.JudySpec.spec
